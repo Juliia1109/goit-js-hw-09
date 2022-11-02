b76f3254a0da061cb9e04d02effaabbb1ffa6a47
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-11-02 23:40:50 +0200

    Завдання 1 , 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+});
